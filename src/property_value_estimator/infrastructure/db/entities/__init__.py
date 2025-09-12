d54f15dfaab177b654288a4a92a45521003fe6f7
FarDust <gabriel.faundez@gmail.com>
2025-09-12 14:07:02 -0300

    feat(db): create migrations of raw tables

--- src/property_value_estimator/infrastructure/db/entities/__init__.py
+++ src/property_value_estimator/infrastructure/db/entities/__init__.py
@@ -1,6 +1,3 @@
 from ..base import Base
-from .house_sales import HouseSales
-from .zipcode_demographics import ZipcodeDemographics
-from .future_house_example import FutureHouseExample
 
-__all__ = ["Base", "HouseSales", "ZipcodeDemographics", "FutureHouseExample"]
+__all__ = ["Base"]
